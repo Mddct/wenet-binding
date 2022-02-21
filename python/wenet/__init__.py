450eb3106da1b417aaec2eed683f1dcbfb9330b8
Mddct <hamddct@gmail.com>
2022-02-21 16:56:21 +0800

    remove ld libray path

--- python/wenet/__init__.py
+++ python/wenet/__init__.py
@@ -1,4 +1,22 @@
+from os import path
+from ctypes import cdll
 
-from lib._pywrap_wenet import Params
-from model import WenetWrapper, load_model  
+import glob
 
+root_path = path.dirname(__file__)
+libs_dirname = path.join(root_path, 'lib')
+
+libs = [
+	"libtorch.so",
+	"libtorch_cpu.so",
+	"libc10.so",
+	"libfst.so.8",
+	"libgomp-a34b3233.so.1",
+]
+
+
+for lib in libs:
+    cdll.LoadLibrary(path.join(libs_dirname, lib))
+
+from wenet.lib._pywrap_wenet import Params
+from wenet.model import WenetWrapper, load_model
