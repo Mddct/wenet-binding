b58433577f85d3901d9cbc04c43d92fb5c5ffb87
mddct <mddct@mddcts-MacBook-Pro.local>
2022-02-23 19:32:38 +0800

    python sphinx-doc google  style  support

--- python/wenet/model.py
+++ python/wenet/model.py
@@ -9,6 +9,8 @@ from wenet.lib._pywrap_wenet import Params, SimpleAsrModelWrapper
 
 
 class WenetWrapper(object):
+    """WenetWrapper is a class for wenet runtime wrapper
+    """
     def __init__(self, params:Params):
         if not params.model_path:
             raise FileNotFoundError("final.zip not found in {}".format(params.model_path))
@@ -21,17 +23,16 @@ class WenetWrapper(object):
     def recognize(
             self,
             filepath: str,
-            nbest: int = 1):
-        """
-            Args:
+            nbest: int = 1) -> str:
 
+        """
+
+        Args:
             filepath (path-like object or file-like object):
-            Source of audio data.
-
-            Returns:
-            res (str)
+            nbest (int): (default: 1)
+        Returns:
+            str:
         """
-
         with wave.open(filepath, "rb") as f:
             assert f.getnchannels() == 1
             assert f.getframerate() == self.params.sample_rate
@@ -41,7 +42,19 @@ class WenetWrapper(object):
         return self.model.recognize(wav_bytes, length, nbest)
 
 
-def load_model(model_dir=None, language=None, cache_dir=pathlib.Path.home()):
+def load_model(model_dir: str=None,
+               language:  str=None,
+               cache_dir: os.PathLike=pathlib.Path.home()) -> str:
+    """
+
+    Args:
+        model_dir (str): (default: None)
+        language (str): (default: None)
+        cache_dir (str): (default: pathlib.Path.home())
+    Returns:
+        str:
+    """
+
     params = Params()
     if model_dir:
         if not os.path.exists(os.path.join(model_dir,"final.zip")):
