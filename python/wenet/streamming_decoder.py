c8216c6ac5bab6ed129ba9a1ce8b9aa6b5780311
mddct <hamddct@gmail.com>
2022-03-09 19:48:23 +0800

    add python streamming

---
+++ python/wenet/streamming_decoder.py
@@ -0,0 +1,35 @@
+import model
+from wenet.lib._pywrap_wenet import (Params, SimpleAsrModelWrapper,
+                                     StreammingAsrWrapper)
+
+
+class StreammingAsrDecoder:
+    '''StreammingAsrDecoder is a class for
+    streamming decoding given a model
+    '''
+    def __init__(self,
+                 model : model.WenetWrapper,
+                 nbest :int = 1,
+                 continuous_decoding :bool= False):
+        self.streamming_decoder = StreammingAsrWrapper(model.model, nbest, continuous_decoding)
+        self.continuous_decoding = continuous_decoding
+
+    def AcceptWaveform(self,
+                       pcm :bytes,
+                       final :bool=True):
+        if len(pcm) == 0:
+            return
+        self.streamming_decoder.AcceptWaveform(pcm, len(pcm), final)
+
+    def GetInstanceResult(self):
+        while True:
+            result = ""
+            final = self.streamming_decoder.GetInstanceResult(result)
+
+            yield result
+
+            if final:
+                self.Reset(1, self.continuous_decoding)
+                break
+    def Reset(self, nbest :int =1 , continuous_decoding :bool = False):
+        self.streamming_decoder.Reset(nbest, continuous_decoding)
